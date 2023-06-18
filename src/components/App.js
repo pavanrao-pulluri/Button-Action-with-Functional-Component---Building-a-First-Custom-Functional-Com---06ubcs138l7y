773ffde3b98d90c089a8323d98b9f8de33f892ba
pavanrao-pulluri <124922581+pavanrao-pulluri@users.noreply.github.com>
2023-06-18 11:08:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [text,setText] = useState("")
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")}>click</button>
+    <pid="para">{text}</p>
     </div>
   );
 }
