b8cf255db9932a0c4aeb87494912b5f873c9701d
pavanrao-pulluri <124922581+pavanrao-pulluri@users.noreply.github.com>
2023-06-18 11:13:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
+
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
   const [text,setText] = useState("")
+  const handleClick=()=>{
+    setText(Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy);
+  }
   return (
     <div id="main">
-      <button id="click" onClick={setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")}>click</button>
+      <button id="click" onClick={handleClick}>click</button>
     <pid="para">{text}</p>
     </div>
   );
