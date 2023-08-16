f82fcc304ac32b04882ca9c763e5a4f258713f91
pavanrao-pulluri <124922581+pavanrao-pulluri@users.noreply.github.com>
2023-08-16 21:29:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,22 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [text,setText] = useState("")
-  const handleClick=()=>{
-    setText(Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy);
-  }
+  const [showPara, setShowPara] = useState(false);
+
+  const handleClick = () => {
+    setShowPara(true);
+  };
+
   return (
-    <div id="main">
-      <button id="click" onClick={handleClick}>click</button>
-    <p id="para">{text}</p>
+    <div className="App">
+      <button id="click" onClick={handleClick}>
+        Click Me
+      </button>
+      {showPara && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+        </p>
+      )}
     </div>
   );
 }
